cbe6217390aac110f7e0733a6db6fd30fc105edd
Ben Colety <ben.colety@gmail.com>
2022-03-03 11:27:00 +0100

    task 6 O3

--- TP_02/part_1/test.s
+++ TP_02/part_1/test.s
@@ -22,17 +22,7 @@ main:
 	@ args = 0, pretend = 0, frame = 0
 	@ frame_needed = 0, uses_anonymous_args = 0
 	@ link register save eliminated.
-	movs	r0, #0
-	mov	r3, r0
-	movs	r2, #9
-.L2:
-	add	r0, r0, r3
-	adds	r3, r3, #1
-	add	r1, r2, #36
-	cmp	r3, #10
-	add	r0, r0, r1
-	add	r2, r2, #9
-	bne	.L2
+	mov	r0, #900
 	bx	lr
 	.size	main, .-main
 	.ident	"GCC: (GNU Tools for Arm Embedded Processors 7-2017-q4-major) 7.2.1 20170904 (release) [ARM/embedded-7-branch revision 255204]"
