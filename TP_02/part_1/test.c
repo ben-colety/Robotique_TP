9ef045393acaec176ec408f6f873d3f0d270f706
Ben Colety <ben.colety@gmail.com>
2022-03-03 11:04:06 +0100

    end of task 5

--- TP_02/part_1/test.c
+++ TP_02/part_1/test.c
@@ -1,7 +1,8 @@
 int main(){
-  int i,j,out = 0;
+  int i,j,out = 0,k;
+  double hello;
   for(i=0;i<10;i++)
     for(j=0;j<10;j++)
       out += i+j;
-//  return out;
+  //return out; for some reason, the return-type warning is not thrown when the warning is active...
 }
--- TP_02/part_1/test.i
+++ TP_02/part_1/test.i
@@ -3,7 +3,8 @@
 # 1 "<command-line>"
 # 1 "test.c"
 int main(){
-  int i,j,out = 0;
+  int i,j,out = 0,k;
+  double hello;
   for(i=0;i<10;i++)
     for(j=0;j<10;j++)
       out += i+j;
