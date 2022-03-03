0219886475f0448bfb5d705f009e2a59180ab7a5
Ben Colety <ben.colety@gmail.com>
2022-03-03 11:15:42 +0100

    task 6 O1

--- TP_02/part_1/test.s
+++ TP_02/part_1/test.s
@@ -5,7 +5,7 @@
 	.eabi_attribute 24, 1
 	.eabi_attribute 25, 1
 	.eabi_attribute 26, 1
-	.eabi_attribute 30, 6
+	.eabi_attribute 30, 1
 	.eabi_attribute 34, 1
 	.eabi_attribute 18, 4
 	.file	"test.c"
@@ -18,48 +18,20 @@
 	.fpu softvfp
 	.type	main, %function
 main:
-	@ args = 0, pretend = 0, frame = 16
-	@ frame_needed = 1, uses_anonymous_args = 0
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
 	@ link register save eliminated.
-	push	{r7}
-	sub	sp, sp, #20
-	add	r7, sp, #0
-	movs	r3, #0
-	str	r3, [r7, #4]
-	movs	r3, #0
-	str	r3, [r7, #12]
-	b	.L2
-.L5:
-	movs	r3, #0
-	str	r3, [r7, #8]
-	b	.L3
-.L4:
-	ldr	r2, [r7, #12]
-	ldr	r3, [r7, #8]
-	add	r3, r3, r2
-	ldr	r2, [r7, #4]
-	add	r3, r3, r2
-	str	r3, [r7, #4]
-	ldr	r3, [r7, #8]
-	adds	r3, r3, #1
-	str	r3, [r7, #8]
-.L3:
-	ldr	r3, [r7, #8]
-	cmp	r3, #9
-	ble	.L4
-	ldr	r3, [r7, #12]
-	adds	r3, r3, #1
-	str	r3, [r7, #12]
+	movs	r2, #9
+	movs	r0, #0
+	mov	r3, r0
 .L2:
-	ldr	r3, [r7, #12]
-	cmp	r3, #9
-	ble	.L5
-	ldr	r3, [r7, #4]
-	mov	r0, r3
-	adds	r7, r7, #20
-	mov	sp, r7
-	@ sp needed
-	pop	{r7}
+	add	r1, r2, #36
+	add	r0, r0, r3
+	add	r0, r0, r1
+	adds	r3, r3, #1
+	adds	r2, r2, #9
+	cmp	r3, #10
+	bne	.L2
 	bx	lr
 	.size	main, .-main
 	.ident	"GCC: (GNU Tools for Arm Embedded Processors 7-2017-q4-major) 7.2.1 20170904 (release) [ARM/embedded-7-branch revision 255204]"
