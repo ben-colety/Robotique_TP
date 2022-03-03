4f448c6cc92d10e0a066c7fa1c1c4a50df9f384d
Ben Colety <ben.colety@gmail.com>
2022-03-03 11:26:36 +0100

    task 6 O2

--- TP_02/part_1/test.s
+++ TP_02/part_1/test.s
@@ -5,12 +5,13 @@
 	.eabi_attribute 24, 1
 	.eabi_attribute 25, 1
 	.eabi_attribute 26, 1
-	.eabi_attribute 30, 1
+	.eabi_attribute 30, 2
 	.eabi_attribute 34, 1
 	.eabi_attribute 18, 4
 	.file	"test.c"
-	.text
+	.section	.text.startup,"ax",%progbits
 	.align	1
+	.p2align 2,,3
 	.global	main
 	.syntax unified
 	.thumb
@@ -21,16 +22,16 @@ main:
 	@ args = 0, pretend = 0, frame = 0
 	@ frame_needed = 0, uses_anonymous_args = 0
 	@ link register save eliminated.
-	movs	r2, #9
 	movs	r0, #0
 	mov	r3, r0
+	movs	r2, #9
 .L2:
-	add	r1, r2, #36
 	add	r0, r0, r3
-	add	r0, r0, r1
 	adds	r3, r3, #1
-	adds	r2, r2, #9
+	add	r1, r2, #36
 	cmp	r3, #10
+	add	r0, r0, r1
+	add	r2, r2, #9
 	bne	.L2
 	bx	lr
 	.size	main, .-main
